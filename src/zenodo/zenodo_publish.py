0ceb459036c21892cc53f618c1c162c0baa8bdce
Generoso Pagano <generoso.pagano@inria.fr>
2015-03-12 15:43:47 +0100

    publish-script

--- src/zenodo/zenodo_publish.py
+++ src/zenodo/zenodo_publish.py
@@ -1,63 +1,87 @@
 # Author: Generoso Pagano
-# Description: this script publish a file on Zenodo
+# Description: script to publish a file on Zenodo
 
 import requests
 import json
 import argparse
 import os.path
 
-# CONSTANTS
+# Constants
 DIR=os.path.dirname(os.path.realpath(__file__)) # Script directory
 ZENODO_CONF_FILE=DIR+"/zenodo_conf.json"
 DEPOSIT_URL="https://zenodo.org/api/deposit/depositions"
 
-# Load Zenodo User's Configuration
-if os.path.isfile(ZENODO_CONF_FILE) == False :
-    with open(ZENODO_CONF_FILE, 'wb') as f:
-        json.dump({'user': 'Doe, John', 'affiliation':'NASA', 'token': 'this-is-a-dummy-token'}, f)
-    print "Configuration file " + ZENODO_CONF_FILE + " not found."
-    print "A default one has been created. Please edit the values."
-    exit()
-f = open(ZENODO_CONF_FILE, 'r')
-config = json.load(f)
-f.close()
-name=config['user']
-affiliation=config['affiliation']
-token=config['token']
-
-# Parse command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("filepath", help="File path")
-parser.add_argument("-t", "--title", help="File title")
-parser.add_argument("-d", "--description", help="File description")
-parser.add_argument("-v", "--verbose", help="Enable verbose output", action="store_true")
-parser.add_argument("-n", "--nopub", help="Create metadata without publishing the file", action="store_true")
-args = parser.parse_args()
-print args
-dry = args.nopub
-title=args.title
-description=args.description
+# Utilities
+def parse_arguments():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath", help="File path")
+    parser.add_argument("-t", "--title", help="File title")
+    parser.add_argument("-d", "--description", help="File description")
+    parser.add_argument("-v", "--verbose", help="Enable verbose output", action="store_true")
+    parser.add_argument("-n", "--nopub", help="Create metadata without publishing the file (dry run)", action="store_true")
+    return parser.parse_args()
 
 def zenodo_api_check(url, token):
     """Verify the correct access to the API"""
     r = requests.get("%s?access_token=%s" % (url, token))
     print r.status_code
     if r.status_code != 200:
-        print "Impossible to use the REST API. Check the content of " + ZENODO_CONF_FILE + "."
+        print("Impossible to use the REST API. Check the content of '" + ZENODO_CONF_FILE + "'.")
         return False
     print "REST API access: OK"
     return True
 
-print zenodo_api_check(DEPOSIT_URL, token)
+def load_user_conf():
+    """ Load Zenodo User's Configuration"""
+    if os.path.isfile(ZENODO_CONF_FILE) == False :
+        with open(ZENODO_CONF_FILE, 'wb') as f:
+            json.dump({'user': 'Doe, John', 'affiliation':'NASA', 'token': 'this-is-a-dummy-token'}, f)
+            print("Configuration file '" + ZENODO_CONF_FILE + "' not found.")
+            print("A default one has been created. PLEASE EDIT THE VALUES!")
+            return None
+    else:
+        f = open(ZENODO_CONF_FILE, 'r')
+        config = json.load(f)
+        f.close()
+        return config
 
-exit()
+def log(do, string):
+    if do:
+        print(string)
 
-############ PUBLISHING PROCEDURE ############
-# After each step, in square brackets I put the reference in the
-# [[https://zenodo.org/dev][documentation]].  The format is:
-# [Section::Function].
 
-# 1. Create a new deposition resource 
+##################### Input Validation  ######################
+# Check user configuration
+config=load_user_config()
+if config == None:
+    exit()
+name=config['user']
+affiliation=config['affiliation']
+token=config['token']
+
+# Parse arguments
+args = parse_arguments()
+verbose = args.verbose
+dry = args.nopub
+title=args.title
+description=args.description
+filepath=args.filepath
+
+# Check REST API
+zenodo_api_check(DEPOSIT_URL, token)
+
+# Check file
+if os.path.isfile(filepath) == False :
+    print("File '"+filepath+"' does not exist.")
+    exit()
+
+##################### PUBLISHING PROCEDURE ######################
+# For each step, in square brackets there is the reference in the
+# documentation (https://zenodo.org/dev).
+# The format is: [Section::Function].
+
+# 1. Create a new deposition resource
 # [Deposition::Create]
 data = {"metadata": 
            {
@@ -70,39 +94,38 @@ data = {"metadata":
        }
 headers = {"Content-Type": "application/json"}
 r = requests.post("%s?access_token=%s" % (url, TOKEN), data=json.dumps(data), headers=headers)
-print "Deposition::Create: " + str(r.status_code)
-print r.json()
+log(verbose, "Deposition::Create: " + str(r.status_code))
 if r.status_code != 201 :
-    print r.json()
+    print(r.json())
     exit()
     
 deposition_id = r.json()['id']
-print "- Deposition Id: " + str(deposition_id)
+log(True, "- Deposition Id: " + str(deposition_id))
 
-# 2. Upload the file 
+# 2. Upload the file
 # [Deposition files::Create(upload)]
-data = {'filename': 'myfirstfile.csv'}
-files = {'file': open('/home/generoso/myfirstfile.csv', 'rb')}
+filename = os.path.basename(filepath)
+data = {'filename': filename}
+files = {'file': open(filepath, 'rb')}
 r = requests.post("%s/%s/files?access_token=%s" % (url, deposition_id, TOKEN), data=data, files=files)
-print "Deposition files::Create(upload): " + str(r.status_code)
-print r.json()
+log(verbose, "Deposition files::Create(upload): " + str(r.status_code))
 if r.status_code != 201 :
-    print r.json()
+    print(r.json())
     exit()
+
+# TEST always DRY
+dry = True
 
 # 3. Actually publishing the file (AFTER THIS STEP THE FILE CAN NOT BE DELETED!!) 
 # [Deposition Actions::Publish]
 if dry == False:
     r = requests.post("%s/%s/actions/publish?access_token=%s" % (url, deposition_id, TOKEN))
-    print "Deposition Actions::Publish: " + str(r.status_code)
-    print r.json()
-    print r.json()['record_url']
-    print r.json()['id']
-    print r.json()['file_id']
+    log(verbose, "Deposition Actions::Publish: " + str(r.status_code))
+    log(verbose, r.json())
+    print("Record Url: " + r.json()['record_url']
+    print("Deposit Id: " + r.json()['id'])
     if r.status_code != 202 :
-        print r.json()
+        print(r.json())
         exit()
 else:
-    print "Dry run: don't publish the file"
-
-############ END OF PROCEDURE ############
+    print("Dry run: don't publish the file")
