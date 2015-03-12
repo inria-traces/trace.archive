d95900c692ebe8717b37f20dfb2ba605ac0cea3e
Luka Stanisic <luka.stanisic@imag.fr>
2015-03-12 14:36:34 +0100

    Adding scripts for running StarPU xp

---
+++ src/capture_metadata.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+# Script for to get machine information before doing the experiment
+
+set +e # Don't fail fast since some information is maybe not available
+
+title="Experiment results"
+starpu_build=""
+host="$(hostname | sed 's/[0-9]*//g' | cut -d'.' -f1)"
+help_script()
+{
+    cat << EOF
+Usage: $0 [options] outputfile.org
+
+Script for to get machine information before doing the experiment
+
+OPTIONS:
+   -h      Show this message
+   -t      Title of the output file
+   -s      Path to the StarPU installation
+EOF
+}
+# Parsing options
+while getopts "t:s:h" opt; do
+    case $opt in
+	t)
+	    title="$OPTARG"
+	    ;;
+	s)
+	    starpu_build="$OPTARG"
+	    ;;
+	h)
+	    help_script
+	    exit 4
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG"
+	    help_script
+	    exit 3
+	    ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+info=$1
+if [[ $# != 1 ]]; then
+    echo 'ERROR!'
+    help_script
+    exit 2
+fi
+starpu_src="$starpu_build/.."
+
+##################################################
+# Preambule of the output file
+echo "#+TITLE: $title" > $info
+echo "#+DATE: $(eval date)" >> $info
+echo "#+AUTHOR: $(eval whoami)" >> $info
+echo "#+MACHINE: $(eval hostname)" >> $info
+echo "#+FILE: $(eval basename $info)" >> $info
+echo " " >> $info 
+
+##################################################
+# Collecting metadata
+echo "* MACHINE INFO:" >> $info
+
+echo "** PEOPLE LOGGED WHEN EXPERIMENT STARTED:" >> $info
+echo "#+BEGIN_EXAMPLE" >> $info    
+who >> $info
+echo "#+END_EXAMPLE" >> $info
+
+echo "** ENVIRONMENT VARIABLES:" >> $info
+echo "#+BEGIN_EXAMPLE" >> $info    
+env >> $info
+echo "#+END_EXAMPLE" >> $info
+
+echo "** HOSTNAME:" >> $info
+echo "#+BEGIN_EXAMPLE" >> $info    
+hostname >> $info
+echo "#+END_EXAMPLE" >> $info
+
+if [[ -n $(command -v lstopo) ]];
+then
+    echo "** MEMORY HIERARCHY:" >> $info
+    echo "#+BEGIN_EXAMPLE" >> $info    
+    lstopo --of console >> $info
+    echo "#+END_EXAMPLE" >> $info
+fi
+
+if [[ -n "$starpu_build" ]]; 
+then
+    echo "** STARPU MACHINE DISPLAY:" >> $info
+    echo "#+BEGIN_EXAMPLE" >> $info    
+    $starpu_build/tools/starpu_machine_display 1> tmp 2> /dev/null
+    cat tmp >> $info
+    rm -f tmp
+    echo "#+END_EXAMPLE" >> $info
+fi
+
+if [ -f /proc/cpuinfo ];
+then
+    echo "** CPU INFO:" >> $info
+    echo "#+BEGIN_EXAMPLE" >> $info    
+    cat /proc/cpuinfo >> $info
+    echo "#+END_EXAMPLE" >> $info
+fi
+
+if [ -f /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor ];
+then
+    echo "** CPU GOVERNOR:" >> $info
+    echo "#+BEGIN_EXAMPLE" >> $info    
+    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor >> $info
+    echo "#+END_EXAMPLE" >> $info
+fi
+
+if [ -f /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq ];
+then
+    echo "** CPU FREQUENCY:" >> $info
+    echo "#+BEGIN_EXAMPLE" >> $info    
+    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq >> $info
+    echo "#+END_EXAMPLE" >> $info
+fi
+
+
+if [[ -n $(command -v nvidia-smi) ]];
+then
+    echo "** GPU INFO FROM NVIDIA-SMI:" >> $info
+    echo "#+BEGIN_EXAMPLE" >> $info    
+    nvidia-smi -q >> $info
+    echo "#+END_EXAMPLE" >> $info
+fi 
+
+if [ -f /proc/version ];
+then
+    echo "** LINUX AND GCC VERSIONS:" >> $info
+    echo "#+BEGIN_EXAMPLE" >> $info    
+    cat /proc/version >> $info
+    echo "#+END_EXAMPLE" >> $info
+fi
+
+if [[ -n $(command -v module) ]];
+then
+    echo "** MODULES:" >> $info
+    echo "#+BEGIN_EXAMPLE" >> $info    
+    module list 2>> $info
+    echo "#+END_EXAMPLE" >> $info
+fi
+
+##################################################
+# Collecting revisions info 
+echo "* CODE REVISIONS:" >> $info
+git_exists=`git rev-parse --is-inside-work-tree`
+if [ "${git_exists}" ]
+then
+    echo "** GIT REVISION OF REPOSITORY:" >> $info
+    echo "#+BEGIN_EXAMPLE" >> $info    
+    git log -1 >> $info
+    echo "#+END_EXAMPLE" >> $info
+fi
+
+svn_exists=`svn info . 2> /dev/null`
+if [ -n "${svn_exists}" ]
+then
+   echo "** SVN REVISION OF REPOSITORY:" >> $info
+   echo "#+BEGIN_EXAMPLE" >> $info    
+   svn info >> $info
+   echo "#+END_EXAMPLE" >> $info
+fi
+
+##################################################
+# Part specific to the StarPU 
+if [[ -n "$starpu_build" ]]; 
+then
+    echo "** SVN REVISION OF ORIGINAL STARPU CODE:" >> $info
+    echo "#+BEGIN_EXAMPLE" >> $info    
+    svn info $starpu_src >> $info    
+    echo "#+END_EXAMPLE" >> $info    
+fi
---
+++ src/starpu/runxp_starpu.sh
@@ -0,0 +1,251 @@
+#!/bin/bash
+# Script for running StarPU experiments
+
+set -e # fail fast
+
+# Script parameters
+basename="$PWD"
+host="$(hostname | sed 's/[0-9]*//g' | cut -d'.' -f1)"
+
+datafolder="/home/stanisic/Repository/trace.archive/data/testing"
+starpu_build="/home/stanisic/Repository/git_gforge/starpu-simgrid/src/StarPU/build-native"
+help_script()
+{
+    cat << EOF
+Usage: $0 options
+
+Script for running StarPU experiments
+
+OPTIONS:
+   -h      Show this message
+   -d      Specify output data folder
+   -s      Path to the StarPU installation
+
+Example how to run it:
+STARPU_NCPU=4 STARPU_NCUDA=0 STARPU_NOPECL=0 STARPU_SIZE=9600 STARPU_BLK=10 STARPU_SCHED=dmda STARPU_CALIBRATE=1 STARPU_PROGRAM=cholesky ./runxp_starpu.sh -d /home/stanisic/Repository/trace.archive/data/testing/ -s /home/stanisic/Repository/git_gforge/starpu-simgrid/src/StarPU/build-native/
+EOF
+}
+# Parsing options
+while getopts "d:s:h" opt; do
+    case $opt in
+	d)
+	    datafolder="$OPTARG"
+	    ;;
+	s)
+	    starpu_build="$OPTARG"
+	    ;;
+	h)
+	    help_script
+	    exit 4
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG"
+	    help_script
+	    exit 3
+	    ;;
+    esac
+done
+info="$datafolder/info.org"
+
+# Reading StarPU command line arguments
+ncpu=$STARPU_NCPU
+if [[ -z $ncpu ]]; then
+    ncpu="4"
+fi
+ncuda=$STARPU_NCUDA
+if [[ -z $ncuda ]]; then
+    ncuda="0"
+fi
+nopencl=$STARPU_NOPENCL
+if [[ -z $nopencl ]]; then
+    nopencl="0"
+fi
+nsize=$STARPU_SIZE
+if [[ -z $nsize ]]; then
+    nsize="9600"
+fi
+nblk=$STARPU_BLK
+if [[ -z $nblk ]]; then
+    nblk="10"
+fi
+starpu_home=$STARPU_HOME
+if [[ -z $starpu_home ]]; then
+    starpu_home=$basename
+fi
+starpu_hostname=$STARPU_HOSTNAME
+if [[ -z $starpu_hostname ]]; then
+    starpu_hostname=$(hostname)
+fi
+starpu_sched=$STARPU_SCHED
+if [[ -z $starpu_sched ]]; then
+    starpu_sched="dmda"
+fi
+starpu_calibrate=$STARPU_CALIBRATE
+if [[ -z $starpu_calibrate ]]; then
+    starpu_calibrate="1"
+fi
+starpu_history_max_error=$STARPU_HISTORY_MAX_ERROR
+if [[ -z $starpu_history_max_error ]]; then
+    starpu_history_max_error="5"
+fi
+starpu_cpuid=$STARPU_WORKERS_CPUID 
+if [[ -z $starpu_cpuid ]]; then
+    starpu_cpuid=""
+else
+    ncpu="$ncpu STARPU_WORKERS_CPUID=\"$STARPU_WORKERS_CPUID\""
+fi
+
+# To choose which program to run
+starpu_program=$STARPU_PROGRAM
+starpu_program_binary="./examples/cholesky/.libs/cholesky_implicit"
+case $starpu_program in
+    'cholesky' | '$starpu_build/examples/cholesky/cholesky_implicit')
+        starpu_program="$starpu_build/examples/cholesky/cholesky_implicit" 
+	starpu_program_binary="$starpu_build/examples/cholesky/.libs/cholesky_implicit"
+        ;;   
+    'lu' | '$starpu_build/examples/lu/lu_example_float')
+        starpu_program="$starpu_build/examples/lu/lu_example_float" 
+	starpu_program_binary="$starpu_build/examples/lu/.libs/lu_example_float"
+        ;;           
+    *)
+        starpu_program="$starpu_build/examples/cholesky/cholesky_implicit" 
+	starpu_program_binary="$starpu_build/examples/cholesky/.libs/cholesky_implicit"
+esac
+
+#################################################################
+# Checking if everything is commited
+if git diff-index --quiet HEAD --; then
+    echo "Everything is commited"
+else
+    echo "ERROR-need to commit all changes before doing experiment!"
+    git status
+    exit 1
+fi
+
+##################################################
+# Exporting FxT path
+case ${host} in
+    'winnetou')
+	export FXT_PATH=/home/stanisic/Repository/FxT
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/stanisic/Repository/FxT/lib
+	;;
+    'paul-bdx' | 'attila' | 'hannibal' | 'conan' | 'mirage' | 'fourmi')
+        ;;
+    'frog' | 'frogkepler' )
+	export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/stanisic/FxT/lib/pkgconfig
+        ;;
+    'pilipili' | 'uv')
+        export FXT_PATH=/home/stanisic/fxt-0.2.11
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/stanisic/fxt-0.2.11/lib
+        ;;
+    'idgraf')
+        export FXT_PATH=/home/lukstanisic/fxt-0.2.11
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/lukstanisic/fxt-0.2.11/lib
+        ;;
+    *)
+        echo "New machine: be sure FxT flags are correct!"
+esac
+
+##################################################
+# Starting to write output file and getting all metadata
+title="StarPU execution on $(eval hostname)"
+../capture_metadata.sh -s $starpu_build -t "$title" $info
+
+##################################################
+# Compilation
+echo "* COMPILATION:" >> $info
+# Configure and make StarPU+Simgrid
+cd $starpu_build
+echo "Make distclean..."
+set +e #If there is nothing to clean
+make -i distclean > /dev/null
+set -e
+# Configure and make StarPU
+echo "** CONFIGURATION OF STARPU:" >> $info
+configopt=""
+case ${host} in
+    'paul-bdx' | 'attila' | 'hannibal' | 'conan' | 'pilipili')
+	;;
+    'winnetou' )
+	;;
+    'fourmi' | 'mirage')
+	configopt="--disable-build-doc --disable-socl --disable-gcc-extensions
+--disable-opencl --with-mkl-cflags=\"-I$MKLROOT/include\" --with-mkl-ldflags=\"-Wl,--start-group $MKLROOT/lib/intel64/libmkl_intel_lp64.so $MKLROOT/lib/intel64/libmkl_sequential.so $MKLROOT/lib/intel64/libmkl_core.so -Wl,--end-group -lpthread -lm\""
+        ;;
+    'frog' | 'frogkepler')
+	configopt="BLAS_LIBS=\"-L/applis/site/stow/gcc_4.4.6/atlas_3.11.17/lib -lsatlas\"" 
+	;;
+    'uv')
+	configopt="--enable-maxcpus=192 --enable-max-sched-ctxs=192 --enable-maxbuffers=192 --with-mkl-cflags=\"-I$MKLROOT/include\" --with-mkl-ldflags=\"-Wl,--start-group $MKLROOT/lib/intel64/libmkl_intel_lp64.so $MKLROOT/lib/intel64/libmkl_sequential.so $MKLROOT/lib/intel64/libmkl_core.so -Wl,--end-group -lpthread -lm\""
+	;;
+    'idgraf')
+	configopt="--disable-build-doc --enable-maxcudadev=8"
+	;;
+    *)
+        echo "New machine: be sure config options are correct!"
+esac
+echo "Configure..."
+echo "#+BEGIN_EXAMPLE" >> $info    
+eval ../configure --prefix=$starpu_build --with-fxt=$FXT_PATH --enable-paje-codelet-details $configopt >> $info
+echo "#+END_EXAMPLE" >> $info
+echo "** COMPILATION OF STARPU:" >> $info
+echo "Make..."
+echo "#+BEGIN_EXAMPLE" >> $info    
+make -j5 >> $info
+make install
+echo "#+END_EXAMPLE" >> $info
+echo "** PROGRAM SCRIPT:" >> $info
+echo "#+BEGIN_EXAMPLE" >> $info
+cat $starpu_program >> $info
+echo "#+END_EXAMPLE" >> $info
+echo "** PROGRAM BINARY LIBRARIES:" >> $info
+echo "#+BEGIN_EXAMPLE" >> $info
+ldd $starpu_program_binary >> $info
+echo "#+END_EXAMPLE" >> $info
+
+##################################################
+# Prepare running options
+cd $datafolder
+running="STARPU_HOME=$starpu_home STARPU_HOSTNAME=$starpu_hostname STARPU_HISTORY_MAX_ERROR=$starpu_history_max_error STARPU_GENERATE_TRACE=1 STARPU_CALIBRATE=$starpu_calibrate STARPU_NCPU=$ncpu STARPU_NCUDA=$ncuda STARPU_NOPENCL=$nopencl STARPU_SCHED=$starpu_sched $starpu_program -size $nsize -nblocks $nblk"
+
+#################################
+# Run application
+echo "* RUNNING OPTIONS:" >> $info
+echo "#+BEGIN_EXAMPLE" >> $info
+echo $running >> $info
+echo "#+END_EXAMPLE" >> $info
+echo "Execute..."
+# Writing results
+rm -f stdout.out
+rm -f stderr.out
+time1=$(date +%s.%N)
+set +e # In order to detect and print execution errors
+eval $running 1> stdout.out 2> stderr.out
+set -e
+time2=$(date +%s.%N)
+echo "* ELAPSED TIME:" >> $info
+echo "#+BEGIN_EXAMPLE" >> $info
+echo "Elapsed:    $(echo "$time2 - $time1"|bc ) seconds" >> $info
+echo "#+END_EXAMPLE" >> $info
+echo "* STDERR OUTPUT:" >> $info
+echo "#+BEGIN_EXAMPLE" >> $info
+cat stderr.out >> $info
+if [ ! -s stdout.out ]; then
+    echo "ERROR DURING THE EXECUTION!!!" >> stdout.out
+fi
+echo "#+END_EXAMPLE" >> $info
+echo "* STDOUT OUTPUT:" >> $info
+echo "#+BEGIN_EXAMPLE" >> $info
+(echo -n "Makespan (in ms): "; cat stdout.out) >> $info
+cat stderr.out
+echo -n "Makespan (in ms): "
+cat stdout.out
+echo "#+END_EXAMPLE" >> $info
+
+##################################
+# StarPU calibration used for this experiment
+echo "* CALIBRATION:" >> $info
+echo "#+BEGIN_EXAMPLE" >> $info
+find $starpu_home/.starpu -not -iwholename '*~' -name *$host* -printf "** File: %f\n%p\n" -exec cat {} \; >> $info
+echo "#! End of calibration" >> $info
+echo "#+END_EXAMPLE" >> $info
